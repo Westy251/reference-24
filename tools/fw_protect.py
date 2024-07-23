443fa5f28514b082339e8d22ac8898dd10de5e9a
Charlie Westwood <Charlie@Beachhurst.com>
2024-07-23 15:28:27 -0400

    copied files over

--- tools/fw_protect.py
+++ tools/fw_protect.py
@@ -1,41 +1,113 @@
 #!/usr/bin/env python
 
-# Copyright 2024 The MITRE Corporation. ALL RIGHTS RESERVED
-# Approved for public release. Distribution unlimited 23-02181-25.
+# Copyright 2023 The MITRE Corporation and team BRUGH!!. ALL RIGHTS RESERVED
+# Approved for public release. Distribution unlimited 23-02181-13.
 
 """
 Firmware Bundle-and-Protect Tool
 
 """
 import argparse
+import random
+from Crypto.Cipher import AES
 from pwn import *
+from Crypto.Hash import SHA256
 
+# Pads the input data using random characters
+# Takes the data to be padded, and the completed size
+# Returns padded data
+def randPad(data, size):
+    # Calculates the number of bytes of padding
+    toPad = size - len(data) % size
 
+    randData = b""
+    # Generates padding
+    for i in range(toPad):
+        randData += p8(random.randint(0, 255), endian = "little")
+
+    return data + randData
+
+# Encrypts the input data using CBC
+# Takes the data to be encrypted, the key,
+# and additional authenticated data
+# Returns the encypted data
+def encrypt(data, key, header):
+    #create hash, but don't send it over yet
+    
+    h = SHA256.new()
+    h.update(data)
+
+    # Returns encrypted data, tag and IV
+    plaintext = data + h.digest()
+    cipher = AES.new(key, AES.MODE_CBC)
+    
+    iv = cipher.iv
+    ct_bytes = cipher.encrypt(plaintext)
+    
+    return(ct_bytes + iv)
+
+# Packages the firmware
+# Takes firmware location, output location,
+# version, release message, and keys location
 def protect_firmware(infile, outfile, version, message):
     # Load firmware binary from infile
-    with open(infile, "rb") as fp:
+    with open(infile, 'rb') as fp:
         firmware = fp.read()
 
-    # Append null-terminated message to end of firmware
-    firmware_and_message = firmware + message.encode() + b"\00"
+    # Instantiate and read the key
+    key = b""
+    header = b""
+    with open ("../bootloader/secret_build_output.txt", "rb") as fp:
+        key = fp.read(16)
+        fp.read(1); # Gets rid of new line between key
+        header = fp.read(16)
 
-    # Pack version and size into two little-endian shorts
-    metadata = p16(version, endian='little') + p16(len(firmware), endian='little')  
+    # Encrypt the firmware
+    messageAndDataEncrypted = b""
+    i = 0
+    messageBin = message.encode()
+    messageBin += b"\x00"
+    firmwareAndMessage = firmware + messageBin #Smushes firmware adnd message together
+    # Breaks into chunks
+    for i in range (0, len(firmwareAndMessage), 1024):
+        # Check if the data fills a full 0x400 chunk
+        if ((len(firmwareAndMessage) - i) // 1024 != 0):
+            temp = p8(2, endian = "little") + encrypt(firmwareAndMessage[i : i + 1024], key, header) # Message type + firmware
+            messageAndDataEncrypted += temp
+    # If the last chunk is not a 0xF chunk, pads and encrypts
+    if (len(firmwareAndMessage) % 1024 != 0):
+        temp = randPad((firmwareAndMessage[i : len(firmwareAndMessage)]), 1024) # Message type + firmware + padding
+        messageAndDataEncrypted += p8(2, endian = "little") + encrypt(temp, key, header)
 
-    # Append firmware and message to metadata
-    firmware_blob = metadata + firmware_and_message
 
-    # Write firmware blob to outfile
-    with open(outfile, "wb+") as outfile:
+    # Create START frame
+    # Temp is the type + version num + firmware len + RM len + padding
+    temp = randPad(p16(version, endian = "little") + p16(len(firmware), endian = "little") + p16(len(messageBin), endian = "little"), 1024)
+    begin = p8(1, endian = "little") + encrypt(temp, key, header)
+
+    # Create END frame
+    # Temp is the type + padding
+    temp = randPad(b"", 1024)
+    end = p8(3, endian = "little") + encrypt(temp, key, header)
+
+    # For debugging?
+    # print(begin)
+    
+    # Smush the START frame, encrypted firmware and RM, and END frame together
+    firmware_blob = begin + messageAndDataEncrypted + end
+    # Write encrypted firmware blob to outfile
+    with open(outfile, 'wb+') as outfile:
         outfile.write(firmware_blob)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Firmware Update Tool")
+    
+# Runs the program
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Firmware Update Tool')
     parser.add_argument("--infile", help="Path to the firmware image to protect.", required=True)
     parser.add_argument("--outfile", help="Filename for the output firmware.", required=True)
     parser.add_argument("--version", help="Version number of this firmware.", required=True)
     parser.add_argument("--message", help="Release message for this firmware.", required=True)
     args = parser.parse_args()
 
-    protect_firmware(infile=args.infile, outfile=args.outfile, version=int(args.version), message=args.message)
+    protect_firmware(infile=args.infile, outfile=args.outfile, version=int(args.version), message=args.message)#Calls the firmware protect method
+    # EXAMPLE COMMAND TO RUN THIS CODE
+    # python3 ./fw_protect.py --infile ../firmware/gcc/main.bin --outfile ../firmware/gcc/protected.bin --version 0 --message lolz
--- tools/fw_update.py
+++ tools/fw_update.py
@@ -1,114 +1,109 @@
 #!/usr/bin/env python
 
-# Copyright 2024 The MITRE Corporation. ALL RIGHTS RESERVED
-# Approved for public release. Distribution unlimited 23-02181-25.
-
-"""
-Firmware Updater Tool
-
-A frame consists of two sections:
-1. Two bytes for the length of the data section
-2. A data section of length defined in the length section
-
-[ 0x02 ]  [ variable ]
---------------------
-| Length | Data... |
---------------------
-
-In our case, the data is from one line of the Intel Hex formated .hex file
-
-We write a frame to the bootloader, then wait for it to respond with an
-OK message so we can write the next frame. The OK message in this case is
-just a zero
-"""
+# Copyright 2023 The MITRE Corporation. ALL RIGHTS RESERVED
+# Approved for public release. Distribution unlimited 23-02181-13.
 
 import argparse
-from pwn import *
 import time
 import serial
 
 from util import *
 
+from Crypto.Util.Padding import pad
+from pwn import *
+
+OK = b"\x00"
+ERROR = b"\x01"
+END = b"\x02"
+
 ser = serial.Serial("/dev/ttyACM0", 115200)
 
-RESP_OK = b"\x00"
-FRAME_SIZE = 256
+FRAME_SIZE = 1073
 
-
+# Sends START frame
+# Takes serial object, meta frame, and debug
 def send_metadata(ser, metadata, debug=False):
-    assert(len(metadata) == 4)
-    version = u16(metadata[:2], endian='little')
-    size = u16(metadata[2:], endian='little')
-    print(f"Version: {version}\nSize: {size} bytes\n")
-
-    # Handshake for update
     ser.write(b"U")
 
     print("Waiting for bootloader to enter update mode...")
     while ser.read(1).decode() != "U":
-        print("got a byte")
+        print("Waiting for response...")
         pass
+    print("Starting upload\n")
+    
+    send_frame(ser, metadata, debug)
 
-    # Send size and version to bootloader.
-    if debug:
-        print(metadata)
-
-    ser.write(metadata)
-
-    # Wait for an OK from the bootloader.
-    resp = ser.read(1)
-    if resp != RESP_OK:
-        raise RuntimeError("ERROR: Bootloader responded with {}".format(repr(resp)))
-
-
+# Sends frames
+# Takes serial object, frame, and debug
 def send_frame(ser, frame, debug=False):
-    ser.write(frame)  # Write the frame...
 
-    if debug:
-        print_hex(frame)
+    falsetimes = 0 # Error counter
+    failed = True # Stores if sent frame was successful
+    
+    # Resend frame if frame fails to send
+    while failed:
+        # Self checks if time-out
+        if falsetimes >= 10:
+            raise RuntimeError("Invalid frame sent too many times, aborting")
+        
+        # Send frame to serial
+        ser.write(frame) 
+        
+        # Get return message type and error number
+        msgType = ser.read(1)
+        errorNum = ser.read(1)
 
-    resp = ser.read(1)  # Wait for an OK from the bootloader
 
-    time.sleep(0.1)
+        time.sleep(0.1)
+        
+        # If debug mode on, prints error type
+        if debug:
+            print("Resp: {}".format(ord(errorNum)))
+            
+        # Check message type
+        if msgType == b'\x04':
+            # Check for success
+            if errorNum == OK:
+                failed = False
+            # Check for error
+            elif errorNum == ERROR:
+                falsetimes += 1 # Increment error counter
+            # Check for end
+            elif errorNum == END:
+                raise RuntimeError("Invalid frame sent too many times, aborting")
+            # Check for invalid error
+            else:
+                raise RuntimeError("Invalid error, aborting")
+        else:
+            raise RuntimeError("Invalid message type, aborting")
 
-    if resp != RESP_OK:
-        raise RuntimeError("ERROR: Bootloader responded with {}".format(repr(resp)))
-
-    if debug:
-        print("Resp: {}".format(ord(resp)))
-
-
+# Sends all frames
+# Takes serial object, encrypted frames location, and debug
+# Returns serial object input
 def update(ser, infile, debug):
-    # Open serial port. Set baudrate to 115200. Set timeout to 2 seconds.
+    # Open and read file of encrypted packets
     with open(infile, "rb") as fp:
         firmware_blob = fp.read()
 
-    metadata = firmware_blob[:4]
-    firmware = firmware_blob[4:]
-
+    # Send START frame
+    metadata = firmware_blob[:1073]
     send_metadata(ser, metadata, debug=debug)
 
+    # Send DATA, MESSAGE, and END frames
+    firmware = firmware_blob[1073:]
     for idx, frame_start in enumerate(range(0, len(firmware), FRAME_SIZE)):
+        # Chunk and write frames
         data = firmware[frame_start : frame_start + FRAME_SIZE]
+        send_frame(ser, data, debug=debug)
+        # Confirm frame has been written
+        print(f"Wrote frame {idx} ({len(data)} bytes)")
 
-        # Construct frame.
-        frame = p16(len(data), endian='big') + data
-
-        send_frame(ser, frame, debug=debug)
-        print(f"Wrote frame {idx} ({len(frame)} bytes)")
-
+    # Print end message
     print("Done writing firmware.")
-
-    # Send a zero length payload to tell the bootlader to finish writing it's page.
-    ser.write(p16(0x0000, endian='big'))
-    resp = ser.read(1)  # Wait for an OK from the bootloader
-    if resp != RESP_OK:
-        raise RuntimeError("ERROR: Bootloader responded to zero length frame with {}".format(repr(resp)))
-    print(f"Wrote zero length frame (2 bytes)")
 
     return ser
 
-
+# Carries out program
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Firmware Update Tool")
 
@@ -117,5 +112,7 @@ if __name__ == "__main__":
     parser.add_argument("--debug", help="Enable debugging messages.", action="store_true")
     args = parser.parse_args()
 
+  
+    # Start updating
     update(ser=ser, infile=args.firmware, debug=args.debug)
-    ser.close()
+
