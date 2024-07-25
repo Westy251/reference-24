b8ce7f3025fc5a1ad7a33ad7fc122a20d5df63ed
Your Name <you@example.com>
2024-07-25 16:31:36 -0400

    adsfkjl;adj

--- bootloader/src/bootloader.c
+++ bootloader/src/bootloader.c
@@ -123,7 +123,7 @@ int main(){
     //debug
     uart_write_str(UART0, "\nseven");
     
-    for (int i = 0; i < size; i++){uart_write(UART0, plaintext[i]);} // Print plaintext
+    for (int i = 0; i < size; i++){uart_write_hex(UART0, plaintext[i]);} // Print plaintext
 
 }
 
