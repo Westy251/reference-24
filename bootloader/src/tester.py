c4c24853bcbb61f4824269a1c2188d799d00cb50
Charlie Westwood <Charlie@Beachhurst.com>
2024-07-29 11:29:32 -0400

    Writes E and n in nice format to terminal

--- bootloader/src/tester.py
+++ bootloader/src/tester.py
@@ -1,17 +1,42 @@
 from Crypto.PublicKey import RSA
+from pwn import *
 import serial
 import re
+import math
 
 ser = serial.Serial("/dev/ttyACM0", 115200)
-
 
 mykey = RSA.generate(2048)
 
 wkey = mykey.export_key(passphrase="Skibidi")
 
-print(wkey)
 
-with open("key.pem", "wb") as brugh:
+e_bytes = mykey.e.to_bytes((mykey.e.bit_length() + 7) // 8, 'little')
+n_bytes = mykey.n.to_bytes((mykey.n.bit_length() + 7) // 8, 'little')
+
+e_converted = "{"
+n_converted = "{"
+
+for i in e_bytes:
+    e_converted += hex(i)
+    e_converted += ", "
+
+for i in n_bytes:
+    n_converted += hex(i)
+    n_converted += ", "
+
+e_converted = e_converted[0:len(e_converted) - 2]
+n_converted = n_converted[0:len(n_converted) - 2]
+
+e_converted += "}"
+n_converted += "}"
+print("e-value: " + e_converted)
+print("")
+print("n-value: " + n_converted)
+
+#print(wkey)
+
+'''with open("key.pem", "wb") as brugh:
     brugh.write(wkey)
 
 def strip_pem_headers(key):
@@ -24,6 +49,12 @@ def strip_pem_headers(key):
 
 wkey = strip_pem_headers(wkey)
 
+n = pack(mykey.n, word_size=len(mykey.n)
+
+
+
+print(n)
+
 with open("key.h", 'w') as f:
     f.write("#ifndef DEFINE_KEY\n")
     f.write(f'static const char KEY[] = "{wkey}";\n')
@@ -31,4 +62,4 @@ with open("key.h", 'w') as f:
 
     #Thanks max
 
-    #convert to der
+    #convert to der'''
